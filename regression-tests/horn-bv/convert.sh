fe2e5f241dd554eb6cb23dc9558bc69a25612f72
Hossein Hojjat <hh@cs.rit.edu>
2018-05-20 14:21:58 -0400

    script

--- regression-tests/horn-bv/convert.sh
+++ regression-tests/horn-bv/convert.sh
@@ -4,5 +4,17 @@ import scala.io.Source
 
 val filename = args(0)
 for (line <- Source.fromFile(filename).getLines) {
-    println(line.replaceAll("Int","(_ BitVec 32)").replaceAll("\\(\\*","(bvmul").replaceAll("(?<!BitVec( | 3))[0-9]+","(_ bv$0 32)"))
+	if (line.replaceAll("\\s","").startsWith(";"))
+		println(line)
+	else
+		println(line.replaceAll("Int","(_ BitVec 32)")
+		.replaceAll("\\*","bvmul")
+		.replaceAll("\\+","bvadd")
+		.replaceAll("(?<![a-zA-Z])-","bvsub")
+		.replaceAll("<=","bvsle")
+		.replaceAll(">=","bvsge")
+		.replaceAll("<","bvslt")
+		.replaceAll("(?<!=)>","bvsgt")
+		.replaceAll("(?<!(BitVec( | 3))|[a-zA-Z])[0-9]+","(_ bv$0 32)")
+		)
 }
