a0b13079ad7f0daafa346b34600c1a6986f0cea5
Philipp Ruemmer <ph_r@gmx.net>
2020-08-25 21:44:18 +0200

    fixed a bug related to sorts of arguments in counterexamples

--- src/lazabs/horn/preprocessor/SymbolSplitter.scala
+++ src/lazabs/horn/preprocessor/SymbolSplitter.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018 Philipp Ruemmer. All rights reserved.
+ * Copyright (c) 2018-2020 Philipp Ruemmer. All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -102,12 +102,13 @@ object SymbolSplitter extends HornPreprocessor {
       def renameFormalArgs(p : Predicate,
                            args : Seq[ITerm]) : Seq[ITerm] = {
         val fixedArgPositions = concreteArgsPerPred(p)
+        val sorts = predArgumentSorts(p)
         (for ((arg, argNum) <- args.iterator.zipWithIndex) yield {
            if (fixedArgPositions contains argNum) {
              // we can use a fresh constant as argument, since
              // the value of the argument is determined by the constraint
              // anyway
-             IConstant(new ConstantTerm (p.name + "_" + argNum))
+             IConstant(sorts(argNum) newConstant (p.name + "_" + argNum))
            } else {
              arg
            }
