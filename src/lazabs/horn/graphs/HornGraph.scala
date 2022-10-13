a9266d525d7055c6169ab1e05008af8459ca59a9
chencheng liang <lcckkkhaha@163.com>
2022-10-13 15:57:21 +0200

    Add initial info

--- src/lazabs/horn/graphs/HornGraph.scala
+++ src/lazabs/horn/graphs/HornGraph.scala
@@ -1,11 +1,34 @@
 package lazabs.horn.graphs
+
+import lazabs.horn.abstractions.VerificationHints
+
 object HornGraphType extends Enumeration {
-  val CDHG,CG = Value
+  val CDHG, CG = Value
 }
 
-class HornGraph {
+case class templateCollection(unlabeled: VerificationHints = VerificationHints(Map()), positive: VerificationHints = VerificationHints(Map()),
+                              negative: VerificationHints = VerificationHints(Map()), predicted: VerificationHints = VerificationHints(Map()))
+
+case class Node(nodeID: Int, canonicalName: String, dotGraphName: String, className: String, shape: String,
+                labelList: Array[Int] = Array(), predictedLabelList: Array[Int] = Array(),
+                color: String = "black", fillColor: String = "while")
 
 
-  
+case class Edge(edge: Array[Int], dotGraphName: String, className: String)
+
+class HornGraph(file: String) {
+
+  val nodeTypes = Seq("relationSymbol", "initial", "false", "relationSymbolArgument", "variables", "operator", "constant", "guard",
+    "clause", "clauseHead", "clauseBody", "clauseArgument")
+  var canonicalClassIDMap: Map[String, Int] = (for (n <- nodeTypes) yield n -> 0).toMap
+  var globalID = 0
+
+  def drawDotGraph(nodeList: Array[Node], edgeMap: Map[String, Array[Edge]]): Unit = {
+
+  }
+
+  def outputJson(): Unit = {
+
+  }
 
 }
