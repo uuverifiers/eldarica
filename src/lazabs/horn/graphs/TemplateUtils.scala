327c93e673d7cc6c65c5b56ff4e6121c21c881c6
chencheng liang <lcckkkhaha@163.com>
2022-10-21 18:36:53 +0200

    get labeled templates

--- src/lazabs/horn/graphs/TemplateUtils.scala
+++ src/lazabs/horn/graphs/TemplateUtils.scala
@@ -1,14 +1,18 @@
 package lazabs.horn.graphs
 
+import ap.SimpleAPI
+import ap.api.SimpleAPI.ProverStatus
+import ap.basetypes.IdealInt
 import ap.parser.IExpression.Eq
 
 import java.io.{File, FileWriter}
-import ap.parser.{IAtom, ITerm, IVariable, Simplifier}
+import ap.parser.{IAtom, IConstant, IExpression, IFormula, IIntLit, IPlus, ISortedVariable, ITerm, ITimes, IVariable, Simplifier, SymbolCollector}
 import ap.terfor.conjunctions.Conjunction
 import ap.terfor.preds.Predicate
 import ap.types.Sort
+import ap.util.Seqs
 import lazabs.GlobalParameters
-import lazabs.horn.abstractions.VerificationHints.{VerifHintTplEqTerm, VerifHintTplInEqTerm, VerifHintTplPredPosNeg}
+import lazabs.horn.abstractions.VerificationHints.{VerifHintElement, VerifHintTplElement, VerifHintTplEqTerm, VerifHintTplInEqTerm, VerifHintTplInEqTermPosNeg, VerifHintTplPred, VerifHintTplPredPosNeg}
 import lazabs.horn.abstractions.{AbsReader, EmptyVerificationHints, LoopDetector, VerificationHints}
 import lazabs.horn.bottomup.DisjInterpolator.AndOrNode
 import lazabs.horn.bottomup.HornClauses.Clause
@@ -19,19 +23,180 @@ import lazabs.horn.graphs.GraphUtils._
 
 object TemplateUtils {
   val sp = new Simplifier
+  val timeoutForPredicateDistinct = 2000 // timeout in milli-seconds used in containsPred
 
   def getTemplateMap(clauses: Clauses): Map[String, VerificationHints] = {
     //notice: templates are only correspond to the original clauses
     val fileTypeList = Seq("unlabeled", "labeled", "predicted", "mined")
-    val unlabeledTemplates = logTime(generateTemplates(clauses),"generate template")
-    writeTemplatesToFile(unlabeledTemplates,"unlabeled")
+    val unlabeledTemplates = logTime(generateTemplates(clauses), "generate template")
+    writeTemplatesToFile(unlabeledTemplates, "unlabeled")
+    val minedTemplates = readTemplateFromFile(clauses, "mined")
+    val labeledTemplates = getLabeledTemplates(unlabeledTemplates, minedTemplates)
+    writeTemplatesToFile(labeledTemplates, "labeled")
+
     (for (t <- fileTypeList) yield t -> readTemplateFromFile(clauses, t)).toMap
   }
 
-  def mineTemplates(simplifiedClauses:Clauses,simpHints:VerificationHints,disjunctive:Boolean,
+  def getLabeledTemplates(unlabeled: VerificationHints, mined: VerificationHints): VerificationHints = {
+    val filterThreshold = 99
+    val filteredMinedTemplates = for ((p, pes) <- mined.predicateHints) yield {
+      val filteredElements = for (pe <- pes; if getVerifHintElementContent(pe)._2 < filterThreshold) yield {
+        pe
+      }
+      p -> filteredElements
+    }
+
+
+    val labeledTemplates = (for ((unlabeledP, unlabeledEs) <- unlabeled.predicateHints) yield {
+      val labeledEs = for (f <- filteredMinedTemplates(unlabeledP); if VerifHintElementContains(unlabeledEs, f)) yield {
+        f
+      }
+      unlabeledP -> labeledEs
+    }).filterNot(x => x._2.isEmpty)
+
+    printListMap(unlabeled.predicateHints, "unlabeledTemplates")
+    printListMap(filteredMinedTemplates, "filteredMinedTemplates")
+    printListMap(labeledTemplates, "labeledTemplates")
+    VerificationHints(labeledTemplates)
+  }
+
+  def VerifHintElementContains(elementList: Seq[VerifHintElement], target: VerifHintElement): Boolean = {
+    var res = false
+    val (targetExpression, _, targetType) = getVerifHintElementContent(target)
+    for (e <- elementList) {
+      val (eExpression, _, eType) = getVerifHintElementContent(e)
+      if (eType == targetType) {
+        e match {
+          case VerifHintTplEqTerm(_, _) => {
+            if (equalTerms(targetExpression.asInstanceOf[ITerm], eExpression.asInstanceOf[ITerm])
+              || equalMinusTerms(targetExpression.asInstanceOf[ITerm], eExpression.asInstanceOf[ITerm]))
+              res = true
+          }
+          case VerifHintTplInEqTerm(_, _) => {
+            if (equalMinusTerms(targetExpression.asInstanceOf[ITerm], eExpression.asInstanceOf[ITerm]))
+              res = true
+          }
+          case VerifHintTplPred(_, _) => {
+            if (containsPred(targetExpression.asInstanceOf[IFormula], Seq(eExpression.asInstanceOf[IFormula])))
+              res = true
+          }
+          case VerifHintTplPredPosNeg(_, _) => {
+            if (containsPred(targetExpression.asInstanceOf[IFormula], Seq(eExpression.asInstanceOf[IFormula])))
+              res = true
+          }
+        }
+      }
+    }
+    res
+  }
+
+  def getVerifHintElementContent(e: VerifHintElement): (IExpression, Int, String) = {
+    e match {
+      case VerifHintTplPred(e, cost) => (e, cost, "VerifHintTplPred")
+      case VerifHintTplPredPosNeg(e, cost) => (e, cost, "VerifHintTplPredPosNeg")
+      case VerifHintTplEqTerm(e, cost) => (e, cost, "VerifHintTplEqTerm")
+      case VerifHintTplInEqTerm(e, cost) => (e, cost, "VerifHintTplInEqTerm")
+      case VerifHintTplInEqTermPosNeg(e, cost) => (e, cost, "VerifHintTplInEqTermPosNeg")
+    }
+  }
+
+  def equalTerms(s: ITerm, t: ITerm): Boolean = {
+    noConstantTerm(s) && noConstantTerm(t) &&
+      (symbols(s + t) forall { c => constVarCoeff(s, c) == constVarCoeff(t, c) })
+  }
+
+  def equalMinusTerms(s: ITerm, t: ITerm): Boolean = {
+    noConstantTerm(s) && noConstantTerm(t) &&
+      (symbols(s + t) forall { c => constVarCoeff(s, c) == -constVarCoeff(t, c) })
+  }
+
+  private def symbols(t: ITerm): Set[ITerm] = {
+    val (vars, consts, _) = SymbolCollector varsConstsPreds t
+    (consts.toSet map IConstant) ++ vars.toSet
+  }
+
+  private def constVarCoeff(t: ITerm, c: ITerm): IdealInt = {
+    assert(c.isInstanceOf[IConstant] || c.isInstanceOf[IVariable])
+    import IExpression._
+    val Sum = SymbolSum(c)
+    t match {
+      case Sum(coeff, _) => coeff
+      case _ => 0
+    }
+  }
+
+  private def noConstantTerm(t: ITerm): Boolean = t match {
+    case _: IIntLit => false
+    case IPlus(a, b) => noConstantTerm(a) && noConstantTerm(b)
+    case ITimes(_, s) => noConstantTerm(s)
+    case _: IConstant | _: IVariable => true
+  }
+
+  private def containsPred(pred: IFormula,
+                           otherPreds: Iterable[IFormula]): Boolean = try {
+    SimpleAPI.withProver { p =>
+      implicit val _ = p
+      import p._
+      import IExpression._
+      var qCounter = 0
+      val predSyms =
+        SymbolCollector.variables(pred) ++ SymbolCollector.constants(pred)
+
+      withTimeout(timeoutForPredicateDistinct) {
+        otherPreds exists {
+          q => {
+            //println(Console.GREEN + "q",qCounter,q)
+            qCounter = qCounter + 1
+            val qSyms =
+              SymbolCollector.variables(q) ++ SymbolCollector.constants(q)
+
+            if (!predSyms.isEmpty && !qSyms.isEmpty &&
+              Seqs.disjoint(predSyms, qSyms)) {
+              // if the predicates do not share any symbols, we can
+              // assume they are not equivalent
+              false
+            } else scope {
+              //val c = pred <=> q
+              val c = (pred <=> q) ||| (pred == q)
+              val vars =
+                SymbolCollector.variables(c)
+
+              // replace variables with constants
+              val varSorts =
+                (for (ISortedVariable(n, s) <- vars.iterator)
+                  yield (n -> s)).toMap
+              val maxVar = if (vars.isEmpty) 0 else
+                (for (IVariable(n) <- vars.iterator) yield n).max
+              val varSubst =
+                (for (n <- 0 to maxVar) yield (varSorts get n) match {
+                  case Some(s) => createConstant(s)
+                  case None => v(n)
+                }).toList
+              ??(subst(c, varSubst, 0))
+              //              println("pred",pred)
+              //              println("vars",vars)
+              //              println("c",c)
+              //              if(??? == ProverStatus.Valid)
+              //                println("pred",pred,"q",q)
+              ??? == ProverStatus.Valid
+            }
+          }
+        }
+      }
+    }
+  } catch {
+    case SimpleAPI.TimeoutException => false
+  }
+
+
+  def mineTemplates(simplifiedClauses: Clauses, simpHints: VerificationHints, disjunctive: Boolean,
                     predGenerator: Dag[AndOrNode[NormClause, Unit]] =>
                       Either[Seq[(Predicate, Seq[Conjunction])],
-                        Dag[(IAtom, NormClause)]]): Unit ={
+                        Dag[(IAtom, NormClause)]]): Unit = {
+    /* notice:
+     different abstract option cause different mined Templates
+     use the abstract option that takes shortest time to solve the problem
+     */
     val counterexampleMethod =
       if (disjunctive)
         CEGAR.CounterexampleMethod.AllShortest
@@ -42,11 +207,13 @@ object TemplateUtils {
         simpHints.toInitialPredicates, predGenerator,
         counterexampleMethod)
     val predMiner = new PredicateMiner(predAbs)
-    val minedTemplates= predMiner
+    val minedTemplates = predMiner.unitTwoVariableTemplates
+    writeTemplatesToFile(minedTemplates, "mined")
+    System.exit(0)
   }
 
   def readTemplateFromFile(clauses: Clauses, templateType: String): VerificationHints = {
-    val fileName = GlobalParameters.get.fileName +"."+ templateType + ".tpl"
+    val fileName = GlobalParameters.get.fileName + "." + templateType + ".tpl"
     if (new File(fileName).exists) {
       val name2Pred =
         (for (Clause(head, body, _) <- clauses.iterator;
@@ -55,7 +222,7 @@ object TemplateUtils {
 
       val readTemplates = readHints(fileName, name2Pred)
       if (GlobalParameters.get.log)
-        printListMap(readTemplates.predicateHints,templateType)
+        printListMap(readTemplates.predicateHints, templateType)
       readTemplates
     } else VerificationHints(Map())
   }
@@ -149,20 +316,20 @@ object TemplateUtils {
     }
   }
 
-  def createNewLogFile(): Unit ={
-    new FileWriter(GlobalParameters.get.fileName+".log", false)
+  def createNewLogFile(): Unit = {
+    new FileWriter(GlobalParameters.get.fileName + ".log", false)
   }
 
-  def logTime[A](input: => A,message:String="")={
-    val start=System.currentTimeMillis()
+  def logTime[A](input: => A, message: String = "") = {
+    val start = System.currentTimeMillis()
     val result = input
     val end = System.currentTimeMillis()
-    writeLog(message + " using time:" + (end-start).toString + " milliseconds")
+    writeLog(message + " using time:" + (end - start).toString + " milliseconds")
     result
   }
 
-  def writeLog(message:String): Unit ={
-    val fw = new FileWriter(GlobalParameters.get.fileName+".log", true)
+  def writeLog(message: String): Unit = {
+    val fw = new FileWriter(GlobalParameters.get.fileName + ".log", true)
     try {
       fw.write(message + "\n")
     }
