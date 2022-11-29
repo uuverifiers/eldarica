4405d8f23168e71e3152811fb0c7ed40f9027908
Zafer Esen <zafer.esen@it.uu.se>
2022-11-29 15:43:47 +0100

    Add licenses to symex.

--- src/lazabs/horn/symex/ConstraintSimplifier.scala
+++ src/lazabs/horn/symex/ConstraintSimplifier.scala
@@ -1,9 +1,42 @@
+/**
+ * Copyright (c) 2022 Zafer Esen, Philipp Ruemmer. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the authors nor the names of their
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package lazabs.horn.symex
 
 import ap.terfor.{ComputationLogger, Term, TermOrder}
 import ap.terfor.arithconj.ModelElement
 import ap.terfor.conjunctions.{ConjunctEliminator, Conjunction}
 
+/**
+ * Takes a constraint and a set of local symbols that can safely be eliminated.
+ * If reduceBeforeSimplification is true, the symbol factory's reducer will
+ * be first applied to the constraint before attempting simplification.
+ */
 trait ConstraintSimplifier {
   def simplifyConstraint(constraint:                 Conjunction,
                          localSymbols:               Set[Term],
@@ -11,6 +44,9 @@ trait ConstraintSimplifier {
       implicit symex_sf:                             SymexSymbolFactory): Conjunction
 }
 
+/**
+ * An implementation of ConstraintSimplifier based on ConjunctEliminator.
+ */
 trait ConstraintSimplifierUsingConjunctEliminator extends ConstraintSimplifier {
 
   class LocalSymbolEliminator(constraint:   Conjunction,
