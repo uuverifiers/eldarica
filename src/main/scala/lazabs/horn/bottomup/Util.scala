de31539c398502c3129e3080b72a0fdb3a8a0b23
Zafer Esen <zafer.esen@it.uu.se>
2023-12-03 18:16:24 +0100

    Moves ClauseTermGraph to horn.bottomup.Util

--- src/main/scala/lazabs/horn/bottomup/Util.scala
+++ src/main/scala/lazabs/horn/bottomup/Util.scala
@@ -1,20 +1,20 @@
 /**
  * Copyright (c) 2011-2022 Philipp Ruemmer. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * * Neither the name of the authors nor the names of their
  *   contributors may be used to endorse or promote products derived from
  *   this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -29,18 +29,23 @@
 
 package lazabs.horn.bottomup
 
+import ap.parser._
+import ap.parser.IExpression.{Eq, i}
+
+import lazabs.horn.bottomup.HornClauses.Clause
 import lazabs.prover.Tree
 
-import scala.collection.mutable.{HashMap => MHashMap}
+import scala.collection.mutable.{
+  ArrayBuffer, HashMap => MHashMap, HashSet => MHashSet}
 
 object Util {
 
   def toStream[A](f : Int => A) : Stream[A] =
     toStreamHelp(0, f)
-  
+
   private def toStreamHelp[A](n : Int, f : Int => A) : Stream[A] =
     f(n) #:: toStreamHelp(n + 1, f)
-  
+
   //////////////////////////////////////////////////////////////////////////////
 
   abstract sealed class Dag[+D] {
@@ -266,7 +271,7 @@ object Util {
             case DagEmpty =>
               (newNext, 0)
           }
-          
+
           (substChildren(subDag)._1, (subDag.size - 1) :: gaps)
         }
       }
@@ -326,7 +331,7 @@ object Util {
   //////////////////////////////////////////////////////////////////////////////
 
   def show[D](d : Dag[D], name : String, reverse : Boolean = true) : Unit = {
-      val runTime = Runtime.getRuntime   
+      val runTime = Runtime.getRuntime
       val filename = if (lazabs.GlobalParameters.get.dotSpec)
                        lazabs.GlobalParameters.get.dotFile
                      else
@@ -339,7 +344,7 @@ object Util {
 
       if (lazabs.GlobalParameters.get.eogCEX) {
         var proc = runTime.exec( "dot -Tpng " + filename + " -o " + filename + ".png" )
-        proc.waitFor    
+        proc.waitFor
         proc = runTime.exec( "eog " + filename + ".png")
 //    proc.waitFor
       }
@@ -368,7 +373,7 @@ object Util {
     def union(d : Int, e : Int) : Unit = {
       val dp = apply(d)
       val ep = apply(e)
-      
+
       if (dp != ep) {
         val dr = rank(dp)
         val er = rank(ep)
@@ -391,6 +396,346 @@ object Util {
     }
 
     override def toString : String = "[" + (parent mkString ", ") + "]"
+  }
+
+  object ClauseTermGraph {
+    case class Edge(from : Node, to : Node)
+
+    abstract class Node
+
+    case class ConstNode(c : IConstant) extends Node {
+      override def toString : String = c.c.name
+    }
+
+    case class AtomNode(a : IAtom) extends Node {
+      val constants : Set[IConstant] =
+        a.args.flatMap(
+          term => SymbolCollector.constants(term).map(c => IConstant(c))).toSet
+      override def toString : String = ap.SimpleAPI.pp(a)
+    }
+
+    case class SyncNode(f : IFormula) extends Node {
+      val constants : Set[IConstant] =
+        SymbolCollector.constants(f).map(c => IConstant(c)).toSet
+      override def toString : String = ap.SimpleAPI.pp(f)
+    }
+
+    // Requires function applications of the form f(\bar{x}) = y.
+    // (Constraint simplifier preprocessor ensures this form.)
+    case class FunAppNode(funApp : IFunApp, eq : IEquation) extends Node {
+      val toArg    : IConstant      = eq.right.asInstanceOf[IConstant]
+      val fromArgs : Set[IConstant] = funApp.args.flatMap(
+        arg => SymbolCollector.constants(arg).map(IConstant)).toSet
+      override def toString : String = ap.SimpleAPI.pp(eq)
+    }
+
+    val emptyOrdering : Ordering[Dag[Node]] = new Ordering[Dag[Node]] {
+      override def compare(x : Dag[Node], y : Dag[Node]) : Int = 0
+    }
+  }
+
+  class ClauseTermGraph(clause : Clause) {
+    import ClauseTermGraph._
+
+    def outgoing(n : Node) = edges.filter(e => e.from == n)
+    def incoming(n : Node) = edges.filter(e => e.to == n)
+
+    private val conjuncts =
+      LineariseVisitor(Transform2NNF(clause.constraint), IBinJunctor.And)
+    private val curNodes = new ArrayBuffer[Node]
+    private val curEdges = new ArrayBuffer[Edge]
+
+    // Add one node for each constant
+    clause.constants.foreach(c => curNodes += ConstNode(IConstant(c)))
+
+    private val sources = clause.body.map(AtomNode)
+    private val sink    = AtomNode(clause.head)
+
+    (sources ++ Seq(sink)) foreach(curNodes += _)
+
+    // Handle clause body
+    for (source <- sources) {
+      // Add each constant as outgoing
+      for (constant <- source.constants) {
+        val constantNode = ConstNode(constant)
+        curEdges += Edge(source, constantNode)
+      }
+    }
+
+    { // Handle clause head / sink
+      // Add each constant as incoming
+      for (constant <- sink.constants) {
+        val constantNode = ConstNode(constant)
+        curEdges += Edge(constantNode, sink)
+      }
+    }
+
+    for (conjunct <- conjuncts) {
+      conjunct match {
+        case IBoolLit(true) => // ignore - used as pseudo-root
+        case Eq(funApp@IFunApp(f, _), _) =>
+          val node = FunAppNode(funApp, conjunct.asInstanceOf[IEquation])
+          for (fromArg <- node.fromArgs) {
+            curEdges += Edge(ConstNode(fromArg), node)
+          }
+          curEdges += Edge(node, ConstNode(node.toArg))
+          curNodes += node
+        case _ =>
+          val node = SyncNode(conjunct)
+          curNodes += node
+          for (constant <- node.constants) {
+            val constantNode = ConstNode(constant)
+            curEdges += Edge(constantNode, node)
+          }
+          // Sync nodes are connected to the sink
+          curEdges += Edge(node, sink)
+      }
+    }
+
+    /**
+     * Returns source nodes, but does not include the pseudo-root and the false
+     * atom. This is useful for getting unbound terms.
+     */
+    val getSources : Iterable[Node] = nodes.filter(
+      node => incoming(node).isEmpty && (node match {
+        case SyncNode(IAtom(p, _)) => p != HornClauses.FALSE
+        case _ => true
+      }))
+
+    // If there is more than one source, add a pseudo-root.
+    // TODO: refactor
+    val (getPseudoRoot, hasPseudoRoot) = if (getSources.size != 1) {
+      val pseudoRoot = SyncNode(i(true))
+      curNodes += pseudoRoot
+      curEdges ++= getSources.map(node => Edge(pseudoRoot, node))
+      (pseudoRoot, true)
+    } else {
+      (getSources.head, false)
+    }
+    // A clause always has a head, but it can be FALSE. Since FALSE doesn't have
+    // any incoming edges, we add these manually.
+    if (clause.head == IAtom(HornClauses.FALSE, Nil)) {
+      val falseNode        = AtomNode(IAtom(HornClauses.FALSE, Nil))
+      val sinksExceptFalse =
+        nodes.filter(node => outgoing(node).isEmpty && node != falseNode)
+      curEdges ++= sinksExceptFalse.map(node => Edge(node, falseNode))
+    }
+
+    def nodes : Iterable[Node] = curNodes
+    def edges : Iterable[Edge] = curEdges
+
+    if (lazabs.GlobalParameters.get.assertions) {
+      // TODO: assert invariants about the graph that should be preserved.
+    }
+
+    /**
+     * Attempts to produce a topologically sorted DAG from the graph.
+     * If the graph has cycles, returns `None`.
+     * @param dagOrdering If possible, DAG will be ordered such that smaller nodes
+     *                 in this ordering will appear earlier in the DAG when
+     *                 possible. For instance, can be used to put selects before
+     *                 stores, which can lead to a DAG with shorter edges.
+     * @return `Option[Dag[Node]]`, `None` if graph has cycles.
+     */
+    def topologicalSort(dagOrdering : Ordering[Dag[Node]] =
+                        ClauseTermGraph.emptyOrdering) : Option[Dag[Node]] = {
+      // Keep track of visited nodes to detect cycles.
+      val createdSubDags = new MHashMap[Node, Dag[Node]]
+      val visiting       = new MHashSet[Node]()
+
+      def buildDagFromNode(node : Node) : Option[Dag[Node]] = {
+        if (visiting.contains(node)) return None // Cycle detected
+        if (createdSubDags.contains(node)) return Some(createdSubDags(node))
+
+        visiting += node
+
+        val childNodes     = outgoing(node).map(edge => edge.to)
+        val maybeChildDags = (for (childNode <- childNodes) yield {
+          buildDagFromNode(childNode)
+        }).toSeq
+
+        if (maybeChildDags.exists(_.isEmpty)) {
+          None
+        } // Cycle detected in one of the child nodes
+        else {
+          // TODO: order children so that the cuts are minimized
+          val childDags           = maybeChildDags.map(_.get).sorted(dagOrdering)
+          val nextDag : Dag[Node] =
+            if (childDags isEmpty) {
+              DagEmpty
+            } else {
+              var next : Dag[Node] = DagEmpty
+              for (subDag <- childDags if !subDag.isEmpty) {
+                next = DagNode(subDag.head, Nil, next)
+              }
+              next
+            }
+
+          visiting -= node
+
+          val dagNode =
+            DagNode(node, childDags.indices.map(_ + 1).toList, nextDag)
+              .substitute(
+                childDags.indices.map(_ + 1).toList.zip(childDags).toMap)
+          createdSubDags += ((node, dagNode))
+
+          Some(dagNode)
+        }
+      }
+
+      buildDagFromNode(getPseudoRoot) match {
+        case Some(dag) => Some(dag.collapseNodes)
+        case None => None
+      }
+    }
+
+    /**
+     * Splits a topologically-sorted DAG into two parts after the specified node.
+     *
+     * @param dag The original DAG to be split.
+     * @param node The node in the DAG after which the split is to occur.
+     * @param glueNodeInstantiator A function to instantiate a new node
+     *                             to serve as a connector between the two parts.
+     *                             Its argument is a list of indices of incoming
+     *                             nodes, and it should return the glue node.
+     * @param isSplittable A predicate to check if the passed node is splittable,
+     *                     i.e., if it can be connected via the connector node.
+     * @param hasPseudoRoot if true, edges coming from the root will not be
+     *                     considered real, and any such orphans will be connected
+     *                     to the glue node below.
+     * @tparam T The type of nodes in the DAG.
+     * @return A tuple containing the two parts of the DAG, in order.
+     */
+    def splitDagAfterNode[T](dag                  : Dag[T],
+                             node                 : T,
+                             glueNodeInstantiator : List[Int] => T,
+                             isSplittable         : T => Boolean,
+                             hasPseudoRoot        : Boolean = false)
+    : (Dag[T], Dag[T]) = {
+      val indexedDag = dag.zipWithIndex
+      val splitInd   = indexedDag.iterator.indexWhere(_._1 == node)
+
+      if (splitInd == -1 || splitInd == dag.size) {
+        throw new IllegalArgumentException(
+          "Node T cannot be the last node or is not found in the DAG")
+      }
+
+      // Index for the glue node
+      val glueInd = splitInd + 1
+
+      /* Sync nodes
+       */
+      val syncsAboveSplit : Seq[DagNode[(T, Int)]] = {
+        indexedDag.subdagIterator.collect{
+          case d@DagNode((SyncNode(_), i), children, _)
+            if children.size == 1 && i + children.head == dag.size - 1 => d
+        }.toSeq
+      }
+
+      val orphanNodeInds             = new ArrayBuffer[(T, List[Int])]
+      val orphanedNodeInds           = new ArrayBuffer[(DagNode[(T, Int)], Int)]
+      val orphanedNodeAboveUpdateMap = new MHashMap[Int, (T, List[Int])]
+      // Orphaned nodes have at least one child below split
+      for (n : DagNode[(T, Int)] <- indexedDag.subdagIterator
+           // only iterate over nodes above the split and ignore pseudo-root
+           if n.d._2 < glueInd && (!hasPseudoRoot || n.d._2 > 0)) {
+        val nInd    = n.d._2
+        val orphans =
+          n.children.filter(childInd => childInd + nInd > splitInd &&
+            !syncsAboveSplit.contains(n))
+            .map(childInd => childInd + nInd - glueInd)
+        if (orphans nonEmpty) {
+          orphanNodeInds += n.d._1 -> orphans
+          orphanedNodeInds += n -> nInd
+          val newChildren = n.children.map(
+            childInd =>
+              if (childInd + nInd > glueInd) glueInd - nInd else childInd)
+            .distinct.sorted
+          orphanedNodeAboveUpdateMap += nInd -> (n.d._1, newChildren)
+        }
+        // Connect syncs above split to the new glue node.
+        if (syncsAboveSplit contains n) {
+          orphanedNodeAboveUpdateMap += nInd -> (n.d._1, List(glueInd - nInd))
+        }
+      }
+
+      /* If there was a pseudo-root with orphaned edges, carry those over to
+         the glue node as outgoing edges.
+       */
+      val orphansFromPseudoRoot =
+        if (hasPseudoRoot) {
+          dag.asInstanceOf[DagNode[T]]
+            .children.filter(childInd => childInd > splitInd)
+        } else {
+          Nil
+        }
+      // Create the glue node using the information of orphaned nodes
+      val glueNode : T = glueNodeInstantiator(orphanedNodeInds.map(_._2).toList)
+
+      val root          = dag.asInstanceOf[DagNode[T]]
+      val dagAboveSplit =
+        DagNode(root.d, root.children.diff(orphansFromPseudoRoot), root.next)
+          .substitute(Map(glueInd -> DagNode(glueNode, List(), DagEmpty)))
+          .updated(orphanedNodeAboveUpdateMap.toMap).elimUnconnectedNodes
+
+      val dagBelowSplit = {
+        val belowOldDag = dag.drop(glueInd)
+        var next        = belowOldDag
+        // orphaned nodes are replicated below
+        for (((orphanedNode, orphanInds), i) <- orphanNodeInds.zipWithIndex) {
+          // offset children by the number of orphans that were added
+          val newChildren = orphanInds.map(ind => ind + i + 1)
+          val newNode     = DagNode(orphanedNode, newChildren, next)
+          next = newNode
+        }
+        val pseudoChildren = orphansFromPseudoRoot.map(
+          childInd => childInd - glueInd + orphanNodeInds.size + 1)
+        DagNode(glueNode, (1 to orphanNodeInds.size).toList ++
+          pseudoChildren, next)
+      }
+
+      //    lazabs.horn.bottomup.Util.show(dag, "dag", false)
+      //    println("\nabove\n")
+      //    lazabs.horn.bottomup.Util.show(dagAboveSplit, "aboveSplit", false)
+      //    dagAboveSplit.prettyPrint
+      //    println("\nbelow\n")
+      //    lazabs.horn.bottomup.Util.show(dagBelowSplit, "belowSplit", false)
+      //    dagBelowSplit.prettyPrint
+      println
+
+      (dagAboveSplit, dagBelowSplit)
+    }
+
+    def show(pngName : String) : Unit = {
+      import java.io.PrintWriter
+
+      val title = clause.toPrologString
+
+      val dotContent = new StringBuilder()
+      dotContent.append("digraph G {\n")
+      dotContent.append(s"""  label="$title";\n""")
+      dotContent.append("  labelloc=t;\n")
+      dotContent.append("  labeljust=l;\n")
+
+      for (edge <- edges) {
+        val from =
+          "\"" + edge.from.toString.replace("\"", "\\\"") + "\""
+        val to =
+          "\"" + edge.to.toString.replace("\"", "\\\"") + "\""
+        dotContent.append(s"  $from -> $to;\n")
+      }
+
+      dotContent.append("}\n")
+
+      val pw = new PrintWriter("graph.dot")
+      pw.write(dotContent.toString)
+      pw.close()
+
+      import sys.process._
+      "dot -Tpng graph.dot -o " + pngName !
+
+      "open " + pngName !
+    }
   }
 
 }
